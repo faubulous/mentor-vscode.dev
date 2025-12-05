2eb965417508d0f32704e69afe63e336e8c85736
Sebastian Faubel <498891+faubulous@users.noreply.github.com>
2025-12-05 17:05:24 +0100

    Initial content adaption.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Mentor',
+			logo: {
+				src: './src/assets/images/mentor.svg',
+				alt: 'Mentor Icon',
+			},
+			favicon: './src/assets/images/mentor.svg',
+			customCss: [
+				'./src/assets/styles/mentor.css',
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/faubulous/mentor-vscode' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: 'Overview',
+					slug: ''
+				}
 			],
 		}),
 	],
